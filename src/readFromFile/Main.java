7876fe6cbde9de681875c3642b66d05d1d225f3b
MihutBalasanu <Mihut.Balasanu@gmail.com>
2019-02-25 10:47:23 +0200

    first commit

---
+++ src/readFromFile/Main.java
@@ -0,0 +1,19 @@
+
+//        Created by IntelliJ IDEA.
+//        User: Mihut BALASANU
+
+
+package readFromFile;
+
+import static readFromFile.ReadFromFile.readFromFile;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        readFromFile("readFromFile/src/Pentalog.txt");
+
+    }
+}
+
+
